7287fbc2a8c42999bee68a59fdc4ef5f124bbbaf
carolinapmd <71374980+carolinapmd@users.noreply.github.com>
2021-01-05 12:02:23 +0000

    implemented firebase storage for contact form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
+import {db} from "./firebase"
 
 import "./ContactForm.css"
 
 export default function ContactForm(){
-      return (  
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+    const[loader, setLoader] = useState(false)
+
+
+function handleNameChange(event){
+    setName(event.target.value)
+}
+function handleEmailChange(event){
+    setEmail(event.target.value)
+}
+function handleMessageChange(event){
+    setMessage(event.target.value)
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    setLoader(true)
+    db.collection("contacts").add({
+        name:name,
+        email:email,
+        message:message,
+    }).then(handleResponse).catch((error)=> {alert(error.message); setLoader(false);
+    });
+
+    setName("")
+    setEmail("")
+    setMessage("")
+};
+
+function handleResponse(){
+    alert("Thanks for contacting! 👍");
+    setLoader(false)
+}   
+
+
+
+return (  
         <div className="ContactForm" >
             <h3 className="contactMe">
                 say <span className="emphasis">
                     hello
                 </span>.
             </h3>
-            <form>
-                <input className="form-control" placeholder="(your full name)" />
-                <input type="email" className="form-control" placeholder="(your email)" />
-                <textarea className="form-control" placeholder="(your message)" rows="4"></textarea>
-                <button type="submit" class="btn btn-primary">Submit</button>
+            <form className="form" onSubmit={handleSubmit}>
+                <input className="form-control nameInput" placeholder="(your full name)" value={name} onChange={handleNameChange} />
+                <input type="email" className="form-control emailInput" placeholder="(your email)" value={email} onChange={handleEmailChange} />
+                <textarea className="form-control messageInput" placeholder="(your message)" value={message} onChange={handleMessageChange}  rows="4"></textarea>
+                <button type="submit" className="btn btn-primary" style={{ background: loader ? "red" : "grey"}}>send</button>
             </form>
         </div>
             
--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,19 @@
 import React from "react";
+import firebase from "firebase"
+
+var firebaseApp= firebase.initializeApp({
+    // Your web app's Firebase configuration
+
+    apiKey: "AIzaSyBqwNyMdovOt2kBiP0eKVJ3T2KnMtuhYNQ",
+    authDomain: "react-contact-form-id.firebaseapp.com",
+    projectId: "react-contact-form-id",
+    storageBucket: "react-contact-form-id.appspot.com",
+    messagingSenderId: "317305343423",
+    appId: "1:317305343423:web:2bf21947fe407d6a0c250a"
+  }
+ 
+)
+
+var db =firebaseApp.firestore();
+
+export {db}
