599277dbe3046a811273bdf586fde2d381c9dc4b
carolinapmd <71374980+carolinapmd@users.noreply.github.com>
2021-01-05 17:04:52 +0000

    minor changes

--- src/PortfolioCard.js
+++ src/PortfolioCard.js
@@ -10,7 +10,7 @@ export default function PortfolioCard(props){
                     <div className="card-body">
                         <h5 className="card-title">{props.role}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <button href="#" className="btn btn-primary">Go somewhere</button>
                     </div>
             </div>
         </div>
