da6aa50be13343e1a8c7a45cc04db51e11164aa8
carolinapmd <71374980+carolinapmd@users.noreply.github.com>
2021-01-06 15:07:40 +0000

    updated Doing component

--- src/Doing.js
+++ src/Doing.js
@@ -1,16 +1,44 @@
 import React from "react";
 import Caroussel from "./Caroussel";
 import Dot from "./img/Dot.png"
+import login from "./img/login.PNG"
+import telaMenu from "./img/telaMenu.PNG"
+import telaOrder from "./img/telaOrder.PNG"
+import modelo from "./img/modelo.PNG"
+import saveLogic from "./img/saveLogic.PNG"
+
 
 import "./Doing.css"
 
 export default function Doing(){
   
-  let One = (<div>hi</div>)
-  let Two = (<div>hi</div>)
-  let Three = (<div>hi</div>)
-  let Four = (<div>hi</div>)
-  let Five = (<div>hi</div>)
+  let One = (
+  <div>
+    <img src={login} alt="login" className="OSPortfolio"/>
+  </div>
+  )
+
+  let Two = (
+  <div>
+    <img src={telaMenu} alt="login" className="OSPortfolio"/>
+  </div>
+  )
+  let Three = (
+  <div>
+    <img src={telaOrder} alt="login" className="OSPortfolio"/>
+  </div>
+  )
+  let Four = (
+  <div>
+    <img src={saveLogic} alt="login" className="OSPortfolio"/>
+  </div>
+  )
+  let Five = (
+  <div>
+    <img src={modelo} alt="login" className="OSPortfolio"/>
+  </div>
+  )
+ 
   
       return (  
         <div className="Doing" >
@@ -30,7 +58,7 @@ export default function Doing(){
                 <span>in the last 4 months i've been passionately building <strong>digital</strong> and <strong>programming</strong> skills; i am currently developing an OS restaurant software for managing orders </span><span className="semicolons">"</span>
               </div>
               <div className="col carousselCol">
-                <Caroussel slideOne={One} slideTwo={Two} slideThree={Three} slideFour={Four} slideFive={Five} slideSix="null" />
+                <Caroussel slideOne={One} slideTwo={Two} slideThree={Three} slideFour={Four} slideFive={Five}  />
               </div>
             </div>
 
